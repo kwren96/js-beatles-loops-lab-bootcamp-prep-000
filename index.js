ee5a63706301171f148ff4d0fb83fc8ec628587a
Katherine Wren <kwren@outlook.com>
2019-03-26 22:40:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // add solution here
+var array = [];
+function theBeatlesPlay(arraymusicians, arrayinstruments) {for (var i = 0; i < 4; i++) {array.push(arraymusicians[i] + ' plays ' + arrayinstruments[i]);}return array}
+function johnLennonFacts(facts) {var i = 0;  while (facts[i]<length.facts) return facts[i] + '!!!'}
