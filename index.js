8ab9116e3bbbb9950e3183c153c2dc30face994d
Katherine Wren <kwren@outlook.com>
2019-03-26 23:06:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // add solution here
 var array = [];
 function theBeatlesPlay(arraymusicians, arrayinstruments) {for (var i = 0; i < 4; i++) {array.push(arraymusicians[i] + ' plays ' + arrayinstruments[i]);}return array}
-function johnLennonFacts(facts) { var i = 0;  while ([i]< facts.length) {(facts[i] + '!!!'); i++ } return}
+function johnLennonFacts(facts) { var repeats = [] var i = 0;  while ([i]< facts.length) {push.repeats(facts[i] + '!!!'); i++ } return repeats}
