2c425fdabf43e3928191fb3549adf2315c1c9525
Katherine Wren <kwren@outlook.com>
2019-03-26 22:52:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // add solution here
 var array = [];
 function theBeatlesPlay(arraymusicians, arrayinstruments) {for (var i = 0; i < 4; i++) {array.push(arraymusicians[i] + ' plays ' + arrayinstruments[i]);}return array}
-function johnLennonFacts(facts) {var i = 0;  while ([i]< facts.length) return (facts[i] + '!!!');}
+function johnLennonFacts(facts) {var i = 0;  while ([i]< facts.length) return (facts[i] + '!!!'); i++}
